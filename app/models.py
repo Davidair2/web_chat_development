067137516f1ae33cba4a60bf442a1eb5f8008e67
21805584 <21805584@student.uwa.edu.au>
2023-05-18 19:38:21 +0800

    initiated db, using different env

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    userinfo = db.relationship('UserInfo', backref='user', lazy='dynamic')
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+    
+class UserInfo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    dob = db.Column(db.DateTime, index=True)
+    country = db.Column(db.String(64))
+    city = db.Column(db.String(64))
+    
+    def __repr__(self):
+        return '<UserInfo {}>'.format(self.first_name)
---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir,'.env'))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('travelapp_secret_key') # to create key, export travelapp_secret_key='your key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
