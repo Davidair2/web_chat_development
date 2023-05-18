3742e76121d3f8d6f2bd69b0db7eb56a31c43589
Davidair2 <81747850+Davidair2@users.noreply.github.com>
2023-05-18 08:55:51 +0800

    Update routes.py
    added the submit function into the routes, submit function get the userinput and send them the ChatGPT

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,30 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, request, make_response, redirect
+from python_chat_with_ChatGPT import get_response
+
 
 @app.route('/')
 def index():
     return render_template('Chat_Page.html')
 
+
 @app.route('/login/')
 def login():
     return render_template('Login_Page.html')
 
+
 @app.route('/register/')
 def register():
     return render_template('Register_page.html')
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    """this is a method that collect the user input from the form when they pressed the 'send' button,
+    this method will send the user input to the ChatGPT api and get a response"""
+    user_input_content = request.form.get('user_input_content')
+    print(user_input_content)
+    GPT_response = get_response(user_input_content)
+    print(GPT_response)
+    return ('/')
+
