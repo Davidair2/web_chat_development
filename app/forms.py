2e3b7c403208b9f3fe01d61af2d27a0498a74d84
21805584 <21805584@student.uwa.edu.au>
2023-05-23 01:29:10 +0800

    small change

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, PasswordField, EmailField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import Email, EqualTo, InputRequired, Length
 from app.models import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    first_name = StringField('First name', validators=[DataRequired()])
-    last_name = StringField('Last name', validators=[DataRequired()])
-    dob = DateField('Date of Birth', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Username', validators=[InputRequired(), Length(min=3, max=12)])
+    email = EmailField('Email', validators=[InputRequired(), Email()])
+    first_name = StringField('First name', validators=[InputRequired(), Length(min=1, max=30)])
+    last_name = StringField('Last name', validators=[InputRequired()])
+    dob = DateField('Date of Birth', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=5, max=10)])
+    password2 = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
     submit = SubmitField('Register')
     
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Sign In')    
